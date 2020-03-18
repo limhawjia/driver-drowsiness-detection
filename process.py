73eda399946dc4c785d60caa8e30a09ee6a1b1f5
limhawjia <hawjiaa@gmail.com>
2020-03-18 11:15:37 +0800

    Change processing to read from nested folders

--- process.py
+++ process.py
@@ -8,32 +8,51 @@ rate = 0.5
 
 
 def process():
+    subfolders = [f.path for f in os.scandir(video_dir) if f.is_dir()]
+    for subfolder in subfolders:
+        process_folder(subfolder)
+
+
+def process_folder(folder_path):
+    print(f'Processing folder: {folder_path}')
+
+    output_path = os.path.join(os.path.realpath(output_dir), get_filename(folder_path))
+    print(f'Extracting videos to: {output_path}')
+
     videos = []
-    for (_, _, files) in os.walk(video_dir):
+    for (_, _, files) in os.walk(folder_path):
         videos.extend(files)
         break
 
     for video in videos:
-        path = os.path.join(os.path.realpath(video_dir), video)
+        path = os.path.join(os.path.realpath(folder_path), video)
         name = get_filename(path)
-        extract_frames(path, name)
+
+        dir_name = os.path.join(os.path.realpath(output_dir), output_path)
+        if not os.path.exists(dir_name):
+            os.makedirs(dir_name)
+
+        extract_frames(path, name, output_path)
 
 
-def extract_frames(video_path, video_name):
+def extract_frames(video_path, video_name, outputpath):
     cap = cv2.VideoCapture(video_path)
 
     if not cap.isOpened():
         print('Unable to open video')
         exit(1)
 
-    dir_name = os.path.join(os.path.realpath(output_dir), video_name)
-    if not os.path.exists(dir_name):
-        os.makedirs(dir_name)
-
     frame_count = 0
     index = 0
-    while cap.isOpened():
-        if index % round(1/rate) != 0:
+
+    print(f'Successfully opened video file: {video_name}, starting frame extraction')
+
+    dir_path = os.path.join(outputpath, video_name)
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+    while cap.isOpened() and index < 20:
+        if index % round(1 / rate) != 0:
             index += 1
             continue
 
@@ -47,7 +66,7 @@ def extract_frames(video_path, video_name):
 
         frame_count += 1
         frame_name = str(frame_count) + '.jpg'
-        frame_path = os.path.join(dir_name, frame_name)
+        frame_path = os.path.join(dir_path, frame_name)
 
         cv2.imwrite(frame_path, frame)
 
@@ -57,8 +76,8 @@ def extract_frames(video_path, video_name):
     cv2.destroyAllWindows()
 
 
-def get_filename(video_path):
-    stripped = os.path.splitext(video_path)[0]
+def get_filename(file_path):
+    stripped = os.path.splitext(file_path)[0]
     name = os.path.split(stripped)[1]
     return name
 
