8583a6f6c7808489f80bfeaa801637d50b9ee625
Joshua Wong <joshwonghc@gmail.com>
2020-04-05 22:03:50 +0800

    Converting code

--- processing/aspect_ratio.py
+++ processing/aspect_ratio.py
@@ -1,5 +1,12 @@
-from scipy.spatial import distance
+import csv
+import os
 import math
+from scipy.spatial import distance
+from facial import get_facial_landmarks
+
+HOME_DIR = '/home/mlsg/driver-drowsiness-detection'
+DISKA_DIR = '/home/mlsg/DiskA'
+DISKB_DIR = '/home/mlsg/DiskB'
 
 # Takes in the landmark
 # Returns two values, the first being the left eye and the second the right eye
@@ -40,3 +47,67 @@ def calc_circularity(coords):
     perimeter += distance.euclidean(coords[5], coords[0])
     return 4 * math.pi * area / (perimeter ** 2)
 
+# File path here is one of the folders containing the 3 videos
+def create_csv_files(file_path, d_name):
+    # Assumes tha the 3 folders are already there
+    dirname = os.path.dirname
+    disk_path = dirname(dirname(file_path))
+    if not os.path.exists(os.path.join(disk_path, 'ratios', d_name)):
+        os.makedirs(os.path.join(disk_path, 'ratios', d_name))
+
+    label_0 = []
+    label_5 = []
+    label_10 = []
+
+    for image in os.listdir(file_path):
+        image_file_path = os.path.join(file_path, image)
+        _, _, result = image.split("_")
+        result = result.split(".")[0]
+        if result == "0":
+            label_0.append(image_file_path)
+        if result == "5":
+            label_5.append(image_file_path)
+        else:
+            label_10.append(image_file_path)
+    create_csv_file(label_0, os.path.join(disk_path, 'ratios', d_name, '0'))
+    create_csv_file(label_5, os.path.join(disk_path, 'ratios', d_name, '5'))
+    create_csv_file(label_10, os.path.join(disk_path, 'ratios', d_name, '10'))
+
+
+def create_csv_file(images, output_path):
+    with open(output_path + '.csv', 'w', newline='') as f:
+        print(output_path)
+        writer = csv.writer(f)
+        for image in images:
+            name, _ = os.path.splitext(image)
+            _, frame, _ = name.split('_')
+            landmarks = get_facial_landmarks(image)
+            if landmarks is None:
+                print("No landmark")
+                continue
+
+            print(image)
+            left_ratio, right_ratio = calc_eyes_aspect_ratio(landmarks)
+            left_circularity, right_circularity = calc_circularities(landmarks)
+            writer.writerow([
+                frame,
+                left_ratio,
+                right_ratio,
+                left_circularity,
+                right_circularity
+            ])
+
+
+if __name__ == "__main__":
+    # Create the output directory for each of the paths
+    if not os.path.exists(os.path.join(DISKA_DIR, 'others', 'ratios')):
+        os.makedirs(os.path.join(DISKA_DIR, 'ratios'))
+
+    if not os.path.exists(os.path.join(DISKB_DIR, 'others', 'ratios')):
+        os.makedirs(os.path.join(DISKB_DIR, 'ratios'))
+
+    for d in os.listdir(os.path.join(DISKA_DIR, 'others', 'faces')):
+        create_csv_files(os.path.join(DISKA_DIR, 'others', 'faces', d), d)
+
+    for d in os.listdir(os.path.join(DISKB_DIR, 'others', 'faces')):
+        create_csv_files(os.path.join(DISKB_DIR, 'others', 'faces', d), d)
--- processing/facial.py
+++ processing/facial.py
@@ -2,9 +2,10 @@ import cv2
 import numpy as np
 import os
 import dlib
+from extractfaces import find_face
 
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+predictor = dlib.shape_predictor("assets/shape_predictor_68_face_landmarks.dat")
 
 for root, dirs, files in os.walk(os.path.join(os.getcwd(), 'faces-training')):
     if len(files) == 0:
@@ -18,21 +19,42 @@ for root, dirs, files in os.walk(os.path.join(os.getcwd(), 'faces-training')):
         print('Facial recognizing ' + file_name)
 
         img = cv2.imread(os.path.join(root, file_name))
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        faces = detector(gray)
-        for face in faces:
-            x1 = face.left()
-            y1 = face.top()
-            x2 = face.right()
-            y2 = face.bottom()
-            #cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+        # faces = detector(gray)
+        # for face in faces:
+            # x1 = face.left()
+            # y1 = face.top()
+            # x2 = face.right()
+            # y2 = face.bottom()
+            # #cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
 
-            landmarks = predictor(gray, face)
+            # landmarks = predictor(gray, face)
 
-            for n in range(0, 68):
-                x = landmarks.part(n).x
-                y = landmarks.part(n).y
-                cv2.circle(img, (x, y), 4, (255, 0, 0), -1)
+        landmarks = get_facial_landmarks(os.path.join(root, file_name))
+        if landmarks is None:
+            continue
+
+        for n in range(0, 68):
+            x = landmarks.part(n).x
+            y = landmarks.part(n).y
+            cv2.circle(img, (x, y), 4, (255, 0, 0), -1)
 
         cv2.imwrite(os.path.join(os.getcwd(), 'facial', file_name), img)
+
+def get_facial_landmarks(file_path):
+    image = cv2.imread(file_path)
+
+    if image is None:
+        print("None")
+        return None
+    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    faces = detector(image)
+
+    if not faces:
+        return None
+
+    face = faces[0]
+    return predictor(image, face)
+
